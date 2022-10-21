900d86d64b93686516141624e8ee49d319891f13
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-21 18:19:24 +0530

    Adding player1 in the game

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,5 +1,9 @@
 public class SnakeAndLadder {
+	static int playerposition;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Program");
+		// uc1: the position of player starting from 0
+		System.out.println("The position of player is: " + playerposition);
+
 	}
 }
