f4e28e260797dfc9739d9503b41939d19695f60c
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-21 19:19:54 +0530

    Adding player 1 rolling the die and completed the game by reaching to position 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -9,26 +9,35 @@ public class SnakeAndLadder {
 		System.out.println("The position of player is: " + playerposition);
 
 		// uc2: the player roll the die for the 1st time.
-		Random r = new Random();
-		int dienumber = r.nextInt(1, 7);
-		System.out.println("player Rolling die rolling and number comes is: " + dienumber);
-		// uc3: checking for option play, no play
-		Random r2 = new Random();
-		int gametime = r2.nextInt(0, 3);
-		System.out.println("snake or ladder checker: " + gametime);
-		switch (gametime) {
-		case 0:
-			playerposition = playerposition + dienumber;
-			System.out.println("Next position of the player" + playerposition);
-			break;
-		case 1:
 
-			playerposition = playerposition - dienumber;
-			System.out.println("Next position of the player: " + playerposition);
-			break;
-		default:
-			System.out.println("No play");
-			break;
+		while (playerposition < 100) {
+			Random r = new Random();
+			int dienumber = r.nextInt(1, 7);
+			System.out.println("player Rolling die rolling and number comes is: " + dienumber);
+			// uc3: checking for option play, no play
+			Random r2 = new Random();
+			int gametime = r2.nextInt(0, 3);
+			System.out.println("snake or ladder checker: " + gametime);
+			switch (gametime) {
+			case 0:
+				playerposition = playerposition + dienumber;
+				System.out.println("Next position of the player: " + playerposition);
+				break;
+			case 1:
+
+				playerposition = playerposition - dienumber;
+				System.out.println("Next position of the player: " + playerposition);
+				break;
+			default:
+				System.out.println("No play");
+				break;
+			}
+			if (playerposition < 0) {
+				playerposition = 0;
+			}
+			System.out.println("final position checker: " + playerposition);
 		}
+		System.out.println("player beat the snakes and completed the game: " + playerposition);
+
 	}
 }
