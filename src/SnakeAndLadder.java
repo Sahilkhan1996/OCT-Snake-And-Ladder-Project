0836ca0533e789c5bdd4766debfb8b89b6380057
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-21 18:22:25 +0530

    Adding player 1 rolling the die for the 1st time

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,16 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
 	static int playerposition;
 
 	public static void main(String[] args) {
+
 		// uc1: the position of player starting from 0
 		System.out.println("The position of player is: " + playerposition);
 
+		// uc2: the player roll the die for the 1st time.
+		Random r = new Random();
+		int dienumber = r.nextInt(1, 7);
+		System.out.println("player Rolling die rolling and number comes is: " + dienumber);
 	}
 }
