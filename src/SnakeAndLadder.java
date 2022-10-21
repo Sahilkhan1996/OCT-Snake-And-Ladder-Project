9371f9a1cae8d351740da3bf73d418a8e89f6b3a
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-10-21 18:51:15 +0530

    Adding player 1 rolling the die and checking the case if play/no play

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -12,5 +12,23 @@ public class SnakeAndLadder {
 		Random r = new Random();
 		int dienumber = r.nextInt(1, 7);
 		System.out.println("player Rolling die rolling and number comes is: " + dienumber);
+		// uc3: checking for option play, no play
+		Random r2 = new Random();
+		int gametime = r2.nextInt(0, 3);
+		System.out.println("snake or ladder checker: " + gametime);
+		switch (gametime) {
+		case 0:
+			playerposition = playerposition + dienumber;
+			System.out.println("Next position of the player" + playerposition);
+			break;
+		case 1:
+
+			playerposition = playerposition - dienumber;
+			System.out.println("Next position of the player: " + playerposition);
+			break;
+		default:
+			System.out.println("No play");
+			break;
+		}
 	}
 }
